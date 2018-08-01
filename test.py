bfc23c9b2ec750b9e203f4b4a62c120cec2c5b63
knighthart <450135440@qq.com>
2018-08-01 19:03:43 +0800

    add:test.py

--- test.py
+++ test.py
@@ -28,13 +28,13 @@ Author:knighthart
                  代码无BUG! 
                  
 """
-#首先我们先导入requests这个包
+# 首先我们先导入requests这个包
 import requests
 
-#我们来吧百度的index页面的html源码抓取到本地，并用r变量保存
-#注意这里，网页前面的 http://一定要写出来，它并不能像真正的浏览器一样帮我们补全http协议
+# 我们来吧百度的index页面的html源码抓取到本地，并用r变量保存
+# 注意这里，网页前面的 http://一定要写出来，它并不能像真正的浏览器一样帮我们补全http协议
 r = requests.get("http://www.baidu.com")
 
 
-#将下载到的内容打印：
+# 将下载到的内容打印：
 print(r.text)
